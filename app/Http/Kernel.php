29888aa17b812eb13100c086665a71f14f2f4af0
Leifer Mendez <leifer33@gmail.com>
2019-05-10 14:53:10 +0200

    Falta lo de e-commerce

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             'throttle:60,1',
             'bindings',
             \Barryvdh\Cors\HandleCors::class,
