8b004181b6b649ceb724862700a33b1634d013fe
Leifer Mendez <leifer33@gmail.com>
2019-08-21 11:22:55 +0200

    email marketing

--- app/Http/Controllers/MailMarketing.php
+++ app/Http/Controllers/MailMarketing.php
@@ -37,8 +37,49 @@ class MailMarketing extends Controller
     public function store(Request $request)
     {
         $users = User::where('status','available')->get();
+
+        $user = [
+            'tester@alterhome.es',
+            's.perini@alterhome.es',
+            's.moutawakil@alterhome.es',
+            's.aliseda@alterhome.es',
+            'rrhh@alterhome.es',
+            'patricia@alterhome.es',
+            'montse@alterhome.es',
+            'luciano@alterhome.es',
+            'libreros@alterhome.es',
+            'leticia@alterhome.es',
+            'hosts@alterhome.es',
+            'facturacion@alterhome.es',
+            'eva@alterhome.es',
+            'chema@alterhome.es',
+            'a.blanco@alterhome.es',
+            'lucas.alterhome@gmail.com',
+            'dinu.alterhome@gmail.com',
+            'marlon.alterhome@gmail.com',
+            'alicia.alterhome@gmail.com',
+            'jfederico.alterhome@gmail.com',
+            'evalunaz.alterhome@gmail.com',
+            'javier.alterhome@gmail.com',
+            'r.alejandro.alterhome@gmail.com',
+            'a.garcia.alterhome@gmail.com',
+            'evaluna.alterhome@gmail.com',
+            'ignacio.alterhome@gmail.com',
+            'cristinar.alterhome@gmail.com',
+            'jorge.alterhome@gmail.com',
+            'raquel.alterhome@gmail.com',
+            'juancarlos.alterhome@gmail.com',
+            'tehui.alterhome@gmail.com',
+            'ingrid.alterhome@gmail.com',
+            'tech.user@alterhome.com',
+            'housekeeping.alterhome@gmail.com',
+            'leifer33@gmail.com'
+        ];
+
         foreach ($users as $user) {
+            $user = User::where('email',$user)->first();
             $user->notify(new _MailMarketing($user));
+            sleep(5);
         }
     }
 
