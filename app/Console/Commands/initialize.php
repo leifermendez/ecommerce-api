2771227597536f3ea3c49b992a8b83e3ce353e6a
Jose G. T <box1488@gmail.com>
2020-03-06 15:58:44 -0500

    comando para  puesta en marcha del sistema

--- app/Console/Commands/initialize.php
+++ app/Console/Commands/initialize.php
@@ -13,44 +13,14 @@ class initialize extends Command
      * @var string
      */
 
-    public function setEnvironmentValue(array $values)
-    {
-
-        $envFile = app()->environmentFilePath();
-        $str = file_get_contents($envFile);
-
-        if (count($values) > 0) {
-            foreach ($values as $envKey => $envValue) {
-
-                $str .= "\n"; // In case the searched variable is in the last line without \n
-                $keyPosition = strpos($str, "{$envKey}=");
-                $endOfLinePosition = strpos($str, "\n", $keyPosition);
-                $oldLine = substr($str, $keyPosition, $endOfLinePosition - $keyPosition);
-
-                // If key does not exist, add it
-                if (!$keyPosition || !$endOfLinePosition || !$oldLine) {
-                    $str .= "{$envKey}={$envValue}\n";
-                } else {
-                    //$str = str_replace($oldLine, "{$envKey}={$envValue}", $str);
-                }
-
-            }
-        }
-
-        $str = substr($str, 0, -1);
-        if (!file_put_contents($envFile, $str)) return false;
-        return true;
-
-    }
-
-    protected $signature = 'initialize {force?}';
+    protected $signature = 'initialize';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Puesta en Marcha';
 
     /**
      * Create a new command instance.
@@ -69,34 +39,127 @@ class initialize extends Command
      */
     public function handle()
     {
-        
-        if ($this->confirm('¿Quieres continuar?')) {
-            $force = $this->argument('force');
-            
-            $values = [
-                'PAACK_KEY' => '',
-                'ELINFORMAR_CLIENT_ID' => '',
-                'ELINFORMAR_CLIENT_SECRET' => '',
-                'TWILIO_SID' => '',
-                'TWILIO_TOKEN' => '',
-                'TWILIO_FROM' => '',
-                'TRUUST_PK' => '',
-                'TRUUST_SK' => '',
-                'STRIPE_KEY' => '',
-                'STRIPE_SECRET' => '',
-                'STRIPE_WEBHOOK_SECRET' => '',
-                'STRIPE_WEBHOOK_TOLERANCE' => '',
-                'STRIPE_PLATFORM_ID' => ''
-            ];
-            $this->setEnvironmentValue($values);
-            
-            if($force=='force'){
-                Artisan::call('key:generate');
-                Artisan::call('jwt:secret');  
-            }
-
+        try {
+            $this->info('Validando la version de PHP');
+            $this->validatePhp();
+            $this->info('PHP correcto');
+            $this->info('Validando Base de Datos');
+            $this->validateBD();
+            $this->info('Base de Datos correcta');
+            $this->info('Inicializando Variables de entorno');
+            $this->configurationEnv();
+            $this->info('Precione Entrer para finalizar');
+            Artisan::call('key:generate');
+            Artisan::call('jwt:secret');
+            $this->info('Porfavor espere mientras se configura la base de datos');
             Artisan::call('migrate:refresh');
+            Artisan::call('db:seed');
+            $this->info('Configuracion completada exitosamente');
+            
+        } catch (Exception $e) {
+            $this->error($e);
+        }
+    }
+
+    public function setEnvironmentValue(array $values){
+        $envFile = app()->environmentFilePath();
+        $str = file_get_contents($envFile);
+
+        if (count($values) > 0) {
+            foreach ($values as $envKey => $envValue) {
+
+                $str .= "\n"; // In case the searched variable is in the last line without \n
+                $keyPosition = strpos($str, "{$envKey}=");
+                $endOfLinePosition = strpos($str, "\n", $keyPosition);
+                $oldLine = substr($str, $keyPosition, $endOfLinePosition - $keyPosition);
+                // If key does not exist, add it
+                if (!$keyPosition || !$endOfLinePosition || !$oldLine) {
+                    $str .= "{$envKey}={$envValue}\n";
+                } else {
+                    $str = str_replace($oldLine, "{$envKey}={$envValue}", $str);
+                }
+            }
         }
 
+        $str = substr($str, 0, -1);
+        if (!file_put_contents($envFile, $str)) return false;
+        return true;
+    }
+
+    public function validatePhp(){
+        $phpv = phpversion();
+        if ($phpv < 7.2) {
+            $this->error('La version PHP debe ser mayor o igual a 7.2');
+            // return false;
+        }
+        //Validando php crul
+        if  (!in_array ('curl', get_loaded_extensions())) {
+            $this->error('Se necesita instalar CRUL en PHP para un funcionamiento correcto');
+        }
+    }
+
+    public function validateBD(){
+        $db = shell_exec('mysql --version');
+        $data = explode(",", $db);
+        if (strpos($data[0], 'Distrib') === false) {
+            $this->error('No tienes base de datos instalada');
+            // return false;
+        }
+        $position = strpos($data[0], 'Distrib');
+        $mysql = substr($data[0], ($position +8),strlen($data[0]));
+        $version = explode("-", $mysql);
+        if (isset($version[1])) {
+            $min = 10.2;
+            $mensaje = 'La version minima de MariaDB debe ser 10.2';
+        }else{
+            $min = 5.6;
+            $mensaje = 'La version minima de MySql debe ser 5.8';
+        }
+        $ver = explode(".", $version[0]);
+        if (($ver[0] .'.'. $ver[1]) < $min) {
+            $this->error($mensaje);
+            // return false;
+        }
+    }
+
+    public function configurationEnv(){
+        $name = getenv('APP_NAME');
+        if ($name == null) {
+            shell_exec('cp .env.example .env');
+        }
+        $this->info('Por favor ingresar los siguientes valores');
+        $DB_HOST = $this->ask('Ingresa el ip del host de la base de datos');
+        $DB_PORT = $this->ask('Ingresa el puerto de base de datos');
+        $DB_DATABASE = $this->ask('Ingresa el nombre de la base de datos');
+        $DB_USERNAME = $this->ask('Ingresa el usuario de la base de datos');
+        $DB_PASSWORD = $this->secret('Ingresa el calve de la base de datos');
+        $values =  [
+            'DB_HOST' => $DB_HOST,
+            'DB_PORT' => $DB_PORT,
+            'DB_DATABASE' => $DB_DATABASE,
+            'DB_USERNAME' => $DB_USERNAME,
+            'DB_PASSWORD' => $DB_PASSWORD,
+            'APP_DEBUG'=>'false',
+            'APP_SITE_MAIL'=>'alterhome',
+            'MAIL_ENCRYPTION'=>'tls',
+            'MAIL_FROM_ADDRESS'=>'from@example.com',
+            'MAIL_FROM_NAME'=>'Example',
+            'PAACK_KEY'=>'3c8244a87b97e6ba9904617951dc10dd340d0b6c',
+            'ELINFORMAR_CLIENT_ID'=>'2ctnq8nqiobu91kpwl0hzwfl3b28x6iwf1f08fyp.api.einforma.com',
+            'ELINFORMAR_CLIENT_SECRET'=>'wl9e1mPswyEMA5txFTY6NlqDndLOzbCleUJux57FE1o',
+            'TWILIO_SID'=>'ACefd4718af2b07d2aa6936d3a37458cb0',
+            'TWILIO_TOKEN'=>'fb6e132e69e1152749032cdf5f5dc27a',
+            'TWILIO_FROM'=>'+34955160684',
+            'TRUUST_PK'=>'pk_production_YXBhdHhlZS1vbi10aW1lLXMtbC0=',
+            'TRUUST_SK'=>'sk_production_VC7sKufCPudxH0Mh6eU51CHX',
+            'STRIPE_KEY'=>'pk_test_iOoJca2tObgjRwE7xbi0T3MM008BdX4xYU',
+            'STRIPE_SECRET'=>'sk_test_G9Qs20pym4a7Nr43SW3F3JDs00lEIQasj9',
+            'STRIPE_WEBHOOK_SECRET'=>'',
+            'STRIPE_WEBHOOK_TOLERANCE'=>'',
+            'STRIPE_PLATFORM_ID'=>'',
+            'GOOGLE_API_VISION_KEY'=>'AIzaSyDAFzd-3P2Wri4m6jgZflzNNUwC5yxjrJ0',
+        ];
+        $this->setEnvironmentValue($values);
     }
 }
+
