8b3969d8f45a06a50cc5aa2c615c70d078e76053
Leifer Mendez <leifer33@gmail.com>
2020-05-10 22:33:20 +0200

    Ready

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,37 +16,37 @@
 
                     <div>
                         <div class="alert alert-warning">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, repellendus!
+                            Proximamente
                         </div>
                     </div>
 
-                        <div class="col-12 p-0 r-0 row">
-                            <div class="col-3">
-                                <div class="card">
-                                    <img class="card-img-top" src="..." alt="Card image cap">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="card">
-                                    <img class="card-img-top" src="..." alt="Card image cap">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
+{{--                        <div class="col-12 p-0 r-0 row">--}}
+{{--                            <div class="col-3">--}}
+{{--                                <div class="card">--}}
+{{--                                    <img class="card-img-top" src="..." alt="Card image cap">--}}
+{{--                                    <div class="card-body">--}}
+{{--                                        <h5 class="card-title">Card title</h5>--}}
+{{--                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--}}
+{{--                                        <a href="#" class="btn btn-primary">Go somewhere</a>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-3">--}}
+{{--                                <div class="card">--}}
+{{--                                    <img class="card-img-top" src="..." alt="Card image cap">--}}
+{{--                                    <div class="card-body">--}}
+{{--                                        <h5 class="card-title">Card title</h5>--}}
+{{--                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--}}
+{{--                                        <a href="#" class="btn btn-primary">Go somewhere</a>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
 
 
-                        </div>
+{{--                        </div>--}}
 
-                    You are logged in!
+{{--                    You are logged in!--}}
                 </div>
             </div>
         </div>
