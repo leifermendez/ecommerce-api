a9eb4901189c9816c50e238964f363f01d82dcf7
Leifer Mendez <leifer33@gmail.com>
2020-05-11 15:18:18 +0200

    antes de public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function() {
+            return base_path().'/template';
+        });
     }
 
     /**
